fffcea8b7cf00d12fc36989893b59fbf24a0a0ca
Nik1406 <n.v.bulatov@mail.ru>
2022-02-03 23:46:47 +1000

    03.02.2022

--- src/Ex01_2/Point3D.java
+++ src/Ex01_2/Point3D.java
@@ -2,12 +2,10 @@ package Ex01_2;
 
 public class Point3D extends Point2D {
     private int z;
-
     public Point3D(int x, int y, int z) {
         super(x, y);
         this.z = z;
     }
-
     public Point3D() {
         this(-1, -1, -1);// вызов конструктора Point3D с параметрами
     }
